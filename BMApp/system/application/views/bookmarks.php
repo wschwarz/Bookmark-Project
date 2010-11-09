da82e90a4f39ac200f13abbba493601673250ed1
wschwarz <wschwarz@ucla.edu>
2010-11-09 01:12:42 -0800

    url updates

--- BMApp/system/application/views/bookmarks.php
+++ BMApp/system/application/views/bookmarks.php
@@ -3,10 +3,10 @@
 	<head>
 		<meta http-equiv="content-type" content="text/html; charset=windows-1250">
 		<title>Bookmarks</title>
-		<link rel="Shortcut Icon" href="../../system/application/styles/Images/1280357233_Red.ico" type="image/x-icon">
-		<link rel="STYLESHEET" type="text/css" href='<?=base_url()?>CodeIgniter/system/application/styles/main.css'>
-		<script type="text/javascript" src="<?=base_url()?>CodeIgniter/system/application/scripts/jquery-1.4.2.min.js"></script>
-		<script type="text/javascript" src="<?=base_url()?>CodeIgniter/system/application/scripts/script.js"></script>
+		<link rel="Shortcut Icon" href="<?=base_url()?>BMApp/system/application/styles/Images/1280357233_Red.ico" type="image/x-icon">
+		<link rel="STYLESHEET" type="text/css" href='<?=base_url()?>BMApp/system/application/styles/main.css'>
+		<script type="text/javascript" src="<?=base_url()?>BMApp/system/application/scripts/jquery-1.4.2.min.js"></script>
+		<script type="text/javascript" src="<?=base_url()?>BMApp/system/application/scripts/script.js"></script>
 	</head>
 	<body>
 		<h1>List of Bookmarks stored for you</h1>
